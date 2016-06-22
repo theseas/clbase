b0bd64f0bf50233bf31468b9e2dc39501506d8f6
theseas <theseas.work@gmail.com>
2016-06-22 10:35:27 +0300

    Added header info 	modified:   header.php 	modified:   index.php

--- header.php
+++ header.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php 
-	wp_head();
-	?>
+	<?php wp_head(); ?>
 </head>
-
+<body>
+	<div class="container">
+		<header>
+			<div>
+				<h1 id="header">
+					<a href=<?php blogninfo('url'); ?><?php bloginfo('name'); ?></a>
+				</h1>
+			</div>
+			<div>
+				<p><?php bloginfo('description'); ?></p>
+			</div>
+		</header>
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
-<body>
 <p>test</p>
 <?php 
 var_dump(hmbase_get_style('style')); 
 ?>
+</div>
 </body>
 </html>
