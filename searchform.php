e4b4942f218d4adc181eeacb09d45a3c4dcc5727
theseas <theseas.work@gmail.com>
2016-06-22 12:39:47 +0300

    Change in serach form button style. 	modified:   searchform.php

--- searchform.php
+++ searchform.php
@@ -7,8 +7,8 @@
 <form role="search" method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="form-group">
         <input type="text" class="form-control" value="<?php echo get_search_query(); ?>" name="s" id="s" />
-		<button type="submit" class="btn btn-primary">
-			<?php echo esc_attr_x('Search', 'submit button'); ?>
+		<button type="submit" class="btn btn-default" aria-label="<?php echo esc_attr_x('Search', 'submit button'); ?>">
+			<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
 		</button>
     </div>
 </form>
