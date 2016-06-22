20e026a03e1c9c175d946a75937717bcdaf3ef51
theseas <theseas.work@gmail.com>
2016-06-22 11:41:59 +0300

    Added test menu and search form 	modified:   header.php 	modified:   index.php

--- header.php
+++ header.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
 <head>
+	<meta value="Content-Type" content="<?php bloginfo('html_type');?> charset=<?php bloginfo('charset');?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
 <body>
 	<div class="container">
 		<header>
-			<div class="col-md-12">
-				<div class="col-md-4">
+			<div class="row">
+				<div class="col-xs-6">
 					<h1 id="header">
-						<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+						<a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
 						<small><?php bloginfo('description'); ?></small>
 					</h1>
 				</div>
 			</div>
-
 		</header>
+		<nav>
+			<div class="row">
+				<div id="menu" class="col-xs-8">
+					<ul>
+						<li><a href="#">test</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-4">
+					<?php echo get_search_form(); ?>
+				</div>
+			</div>
+		</nav>
--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php get_header(); ?>
-<p>test</p>
-<?php 
-var_dump(hmbase_get_style('style')); 
-?>
 </div>
 </body>
 </html>
