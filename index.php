c3a8efa6eb05d33cace98ac62c819589731e49d2
theseas <theseas.work@gmail.com>
2016-06-29 00:31:40 +0300

    Fixed typo in index.php 	modified:   index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ get_header();
 						while(have_posts()){
 							the_post();
 							hmbase_log('$post', $post);
-							hmbase_log('get_post_format', get_post_format);
+							hmbase_log('get_post_format', get_post_format());
 							get_template_part('template_parts/content', get_post_format());
 				?>
 				<?php
