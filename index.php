674a85df8315e5d0275049841fd28a2e847c4902
theseas <theseas.work@gmail.com>
2016-06-28 23:39:02 +0300

    Splitted content into a differend files Added debug info into the loop 	modified:   index.php 	new file:   template_parts/content.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php get_header(); ?>
+<?php
+define('WP_USE_THEMES', false);
+get_header();
+?>
 	<section>
 		<main>
 			<div>
@@ -6,7 +9,8 @@
 					// The Loop Begins
 					if(have_posts()){
 						while(have_posts()){
-							the_post();
+							hmbase_log('the_post', the_post());
+							get_template_part('template_parts/content', get_post_format());
 				?>
 				<?php
 					// The Loop Ends
