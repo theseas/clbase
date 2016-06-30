410110708c9ed6c7d5c9412d811cc613edc7e3c1
theseas <theseas.work@gmail.com>
2016-06-30 09:55:44 +0300

    Moved hmbase_log function fron utils.php to functions.php Created function signature hmbase_make_excerpt 	modified:   functions.php 	modified:   utils.php

--- utils.php
+++ utils.php
@@ -1,17 +1,5 @@
 <?php
 // Utility functions
 
-/*
- * @description Formats and logs an object
- * @param string	$name	Name of the object	 
- * @param mixed		$obj 	Object to be loged
- */
-function hmbase_log($name, $obj){
-	if(WP_DEBUG===true){
-		$obj_str = print_r($obj, true);
-		$obj_str = $name . ': ' .str_replace("\n", '', $obj_str);
-		error_log($obj_str);
-	}
-}
 
 ?>
