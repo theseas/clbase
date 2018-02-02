1e4f7302f03430138a088790b81e88d8980b8fbc
theseas <theseas.work@gmail.com>
2018-02-02 19:44:34 +0200

    In comments.php added edit link and author and website links. 	modified:   comments.php

--- comments.php
+++ comments.php
@@ -19,9 +19,15 @@ if($comments): ?>
 <h3><?php _e('Thoughts on ', 'clbase'); the_title(); ?></h3>
 <?php	foreach($comments as $comment): ?>
 		<div class="comment">
-			<p><a href="<?php echo esc_url(get_comment_autor_link($comment_ID)); ?>"><?php echo $comment->comment_author; ?></a></p>
+			<p>
+				<a href="<?php echo esc_url(bloginfo('url') . '/' . get_comment_author_link($comment_ID)); ?>"><?php echo $comment->comment_author; ?></a>
+				<span><?php echo __("Posted on ", 'clbase') . $comment->comment_date; ?> </span>
+			</p>
 			<p><a rel="nofollow" href="<?php echo esc_url($comment->comment_author_url);?>"><?php echo $comment->comment_author_url; ?></a></p>
 			<p> <?php echo $comment->comment_content; ?> </p>
+			<div class="comment-controls">
+				<span>$edit_comment_link(__('Edit Comment', 'clbase'))</span>
+			</div>
 		</div>
 
 <?php
