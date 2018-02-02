0a20d4ad72994dd0bed6131975efc95d3bf33963
theseas <theseas.work@gmail.com>
2018-02-02 23:14:36 +0200

    In single.php fixed layout issue 	modified:   single.php

--- single.php
+++ single.php
@@ -22,20 +22,21 @@
 						}
 					?>
 				</main>
+				<div class="row">
+					<div id="posts_pagination" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+						<?php echo get_the_posts_pagination(['mid_size'=>1,
+							'prev_text'=>'< '. __('Previous', 'clbase') ,
+							'next_text'=>__('Next', 'clbase') . ' >']);
+						?>
+					</div><!-- END #posts_pagination -->
+				</div><!-- END .row -->
 			</div><!-- END #posts -->
-			<div class="row">
-				<div id="posts_pagination" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-					<?php echo get_the_posts_pagination(['mid_size'=>1,
-						'prev_text'=>'< '. __('Previous', 'clbase') ,
-						'next_text'=>__('Next', 'clbase') . ' >']);
-					?>
-				</div><!-- END #posts_pagination -->
-			</div><!-- END .row -->
+			<?php
+			if(is_active_sidebar('right-sidebar')){
+				get_sidebar();
+			} // endif sidebar
+			?>
 	</div> <!-- END .main -->
-	<?php
-	if(is_active_sidebar('right-sidebar')){
-		get_sidebar();
-	} // end sidebar if ?>
 	<?php get_footer() ?>
 </div>
 </body>
